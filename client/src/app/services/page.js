e55b249d7a16c142ab3c0fff18f66514019ffd48
Gboly <gbolahanahmed1@gmail.com>
2025-08-09 11:56:35 +0100

    Feat: Create Services page. Include a new service

---
+++ client/src/app/services/page.js
@@ -0,0 +1,62 @@
+"use client";
+
+import Image from "next/image";
+import "./page.css";
+import { services, servicesParagraphs } from "@/utils/data";
+
+export default function Services() {
+  return (
+    <main className="services">
+      <section
+        className="services-header"
+        style={{
+          backgroundImage: `url(/services-bg.png)`,
+        }}
+      >
+        <h2>Our Services</h2>
+      </section>
+      <section className="services-body-desc">
+        {servicesParagraphs.map(({ text, image }, index) => (
+          <div
+            className={`service-block ${index % 2 === 1 ? "reverse" : ""}`}
+            key={index}
+          >
+            <div className="service-text">
+              <p>{text}</p>
+            </div>
+            <div className="service-image">
+              <Image
+                src={image}
+                alt={`Abstract ${index + 1}`}
+                width={1640}
+                height={924}
+              />
+            </div>
+          </div>
+        ))}
+      </section>
+      <section className="services-grid">
+        {services.map((service, index) => (
+          <div className="flip-card" key={index}>
+            <div className="flip-card-inner">
+              {/* Front */}
+              <div
+                className="flip-card-front"
+                style={{ backgroundImage: `url(${service.image})` }}
+              >
+                <h3>{service.title}</h3>
+              </div>
+
+              {/* Back */}
+              <div className="flip-card-back">
+                <h3>{service.title}</h3>
+                <p>{service.desc}</p>
+                <button>Discover More</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </section>
+    </main>
+  );
+}
--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -3,30 +3,42 @@ export const services = [
     title: "Financial Stewards Academy",
     image:
       "https://res.cloudinary.com/dirwr8cde/image/upload/v1754088973/Amitt/fsa_mghjft.png",
+    desc: "The Financial Stewards Academy (FSA) is a transformative course designed to equip you with practical financial skills and personal growth strategies. This course helps you feel confident, capable, and committed to your financial goals.",
     link: "/services/fsa",
+  },
+  {
+    title: "Financial Empowerment for Employees",
+    image:
+      "https://res.cloudinary.com/dirwr8cde/image/upload/v1754736236/Amitt/fee_anlwvr.png",
+    desc: "This is a transformative online course designed to equip employees with practical financial skills and personal growth strategies. By addressing financial stress, a leading cause of workplace distraction and turnover, this course helps employees feel confident, capable, and committed to their goals.",
+    link: "/services/fee",
   },
   {
     title: "Customer Service Training",
     image:
       "https://res.cloudinary.com/dirwr8cde/image/upload/v1754088970/Amitt/cst_tcxeit.png",
+    desc: "A professional development program for individuals and corporate teams, designed to build communication skills, enhance customer interactions, and foster service excellence that drives loyalty and business growth.",
     link: "/services/cst",
   },
   {
     title: "Travel and Tours",
     image:
       "https://res.cloudinary.com/dirwr8cde/image/upload/v1754088971/Amitt/travel_ghmtjd.png",
+    desc: "We deliver seamless trip planning, bookings, and curated tour experiences, offering unforgettable journeys for leisure, exploration, and cultural discovery.",
     link: "/services/travel",
   },
   {
     title: "Professional Courses",
     image:
       "https://res.cloudinary.com/dirwr8cde/image/upload/v1754088970/Amitt/courses_jrpkl5.png",
+    desc: "We offer a range of practical and impactful courses, including The Money Playbook, Introduction to Team Leadership, and Leaving Home, designed to equip participants with essential life skills, financial know-how, and leadership capabilities for personal and professional growth.",
     link: "/services/courses",
   },
   {
     title: "Startup Funding Facilitation",
     image:
       "https://res.cloudinary.com/dirwr8cde/image/upload/v1754088971/Amitt/sff_a4tkya.png",
+    desc: "We connect entrepreneurs with the right investors by managing the entire venture capital outreach process, from refining your pitch to securing private equity so you can focus on building and growing your business.",
     link: "/services/sff",
   },
 ];
@@ -72,5 +84,20 @@ export const customMessageDetails = [
   {
     name: "message",
     label: "Your Message To Us *",
+  },
+];
+
+export const servicesParagraphs = [
+  {
+    text: "At Amitt Training and Consulting Limited , we provide a unique blend of training, consulting, and advisory solutions designed to empower individuals, startups, and organizations to achieve sustainable growth. Our services are rooted in practical strategies, hands-on guidance, and real-world experience, ensuring that every client gains both the knowledge and confidence to take decisive steps toward their goals. We combine professional expertise with a people-first approach, delivering results that are impactful, measurable, and lasting.",
+    image: "/service-abstract-1.png",
+  },
+  {
+    text: "Our work spans across multiple focus areas, from financial education to professional skills development, startup funding facilitation, and curated lifestyle experiences. We equip individuals and teams with essential knowledge through programs like our financial literacy academy, customer service training, and personal development courses, while also supporting startup founders in navigating the often complex process of securing venture capital. Beyond professional growth, we provide tailored travel and tour experiences that foster exposure, networking, and cultural enrichment, adding a unique dimension to our holistic approach to development.",
+    image: "/service-abstract-2.png",
+  },
+  {
+    text: "Whether you’re an individual looking to strengthen your skills, a company aiming to upskill your workforce, or a founder ready to take your venture to the next level, our services are built to meet you where you are — and take you further. We don’t just deliver training or connect you to opportunities; we partner with you on the journey, ensuring that each program, consultation, or engagement is aligned with your goals and produces meaningful, long-term results.",
+    image: "/service-abstract-3.png",
   },
 ];

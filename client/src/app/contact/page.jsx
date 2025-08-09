6d302b6e9e8e60c9ba74ab7eadaddc495c5853f5
Gboly <gbolahanahmed1@gmail.com>
2025-08-09 22:20:56 +0100

    fix: Update contact page

--- client/src/app/contact/page.jsx
+++ client/src/app/contact/page.jsx
@@ -6,36 +6,73 @@ import { customMessageDetails } from "@/utils/data";
 
 import CallIcon from "@mui/icons-material/Call";
 import MailIcon from "@mui/icons-material/Mail";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import Image from "next/image";
 
 export default function Contact() {
   return (
     <main className="contact">
       {/* Intro Section */}
       <section className="intro">
-        <img src="/contact.png" alt="Contact" />
-        <div>
-          <div>
-            <header>Get In Touch</header>
-            <p>
-              Whether you're a talented professional or a company seeking the
-              right fit, our team is ready to connect and guide you. Your
-              journey begins with a simple message or call—let's embark on this
-              transformative path together.
-            </p>
-            <aside>
-              <div>
-                <CallIcon />
-                <a href="tel:+447519280650">+447519280650</a>
-              </div>
-              <div>
-                <MailIcon />
-                <a href="mailto:info@amittconsulting.co.uk">
-                  info@amittcsl.com
-                </a>
-              </div>
-            </aside>
-          </div>
+        <div className="intro-content">
+          <header>Get In Touch</header>
+          <p>
+            Whether you're a talented professional or a company seeking the
+            right fit, our team is ready to connect and guide you. Your journey
+            begins with a simple message or call—let's embark on this
+            transformative path together.
+          </p>
+
+          <aside className="contact-info">
+            {/* Column 1 - Location */}
+            <div className="contact-column">
+              <LocationOnIcon style={{ color: "#e63946", fontSize: "2rem" }} />
+              <p>
+                UK: 1st Floor, North Westgate House, Harlow, Essex, England,
+                CM20 1YS.
+              </p>
+              <p>Nigeria: Block 95 Omorinre Johnson Street, Lekki 1, Lagos.</p>
+              <p>
+                Nigeria: AACSL House, 5/7 Isheri Road, Berger, Lagos, Nigeria.
+              </p>
+            </div>
+
+            {/* Column 2 - Phone */}
+            <div className="contact-column">
+              <CallIcon style={{ color: "#1d3557", fontSize: "2rem" }} />
+              <p>+44 7887244035</p>
+              <p>+44 7732687649</p>
+              <p>+234 9046126671</p>
+            </div>
+
+            {/* Column 3 - Email */}
+            <div className="contact-column">
+              <MailIcon style={{ color: "#457b9d", fontSize: "2rem" }} />
+              <p>info@amittcsl.com</p>
+            </div>
+          </aside>
         </div>
+      </section>
+
+      <section className="contact-images">
+        <Image
+          src="/contact-abstract-1.png"
+          alt="Image 1"
+          width={400}
+          height={250}
+        />
+        <Image
+          src="/contact-abstract-2.png"
+          alt="Image 2"
+          width={400}
+          height={250}
+        />
+        <Image
+          src="/contact-abstract-3.png"
+          alt="Image 3"
+          width={400}
+          height={250}
+        />
       </section>
 
       {/* Message Form Section */}
